dcbcca1309021a6f6e654fe8e44258dc329a5ddd
zezomo <zezouniverse7@gmail.com>
2023-07-16 21:11:04 +0000

    push it

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+
 /**
+ * main - entry point
+ *
  * Descrubtion: using printf function
  *
- * Retern: Always 0
+ * Return: Always 0
 */
 
 int main(void)
 {
-	printf("\"Programming is like building a multilingual puzzle.\n");
+	printf("Programming is like building a multilingual puzzle.\n");
 	return (0);
 }
