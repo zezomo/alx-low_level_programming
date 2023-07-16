b0f7db0e16dd93af4437276b3cc9456f5cc2201d
zezomo <zezouniverse7@gmail.com>
2023-07-16 20:57:15 +0000

    push it

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
- * main entry point
- * 
- * description function will be fu
+ * main - Entry point
  *
- * return value will always be 0
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (success)
 */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
