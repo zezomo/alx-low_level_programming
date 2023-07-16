5b71dde7b92d2cd87daacd612eb0e58392b1c550
zezomo <zezouniverse7@gmail.com>
2023-07-16 22:24:13 +0000

    push it

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Descrubtion: using Size function
+ *
+ * Return: Always 1
+*/
+
+int main(void)
+{       char q[] = "and that piece of art is useful/" - Dora Korpar, 2015-10-19\n";
+	write(1, quo, 59);
+        return (1);
+}
