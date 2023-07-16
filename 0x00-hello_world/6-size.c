2003798b8937d7f54667134252df9eba205c3053
zezomo <zezouniverse7@gmail.com>
2023-07-16 21:26:33 +0000

    push it

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@
 
 int main(void)
 {
-	printf("Size of a char",sizeof(char));
-	printf("Size of a int",sizeof(int));
-	printf("Size of a long int",sizeof(long int));
-	printf("Size of a long long int",sizeof(long long int));
-	printf("Size of a float",sizeof(float));
+	printf("Size of a char: %lu byte\n",sizeof(char));
+	printf("Size of a int: %lu byte\n",sizeof(int));
+	printf("Size of a long int: %lu byte\n",sizeof(long int));
+	printf("Size of a long long int: %lu byte\n",sizeof(long long int));
+	printf("Size of a float: %lu byte\n",sizeof(float));
 	return (0);
 }
