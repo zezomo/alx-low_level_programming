84649de30722412f8807d1c72bd774dccb9a5404
zezomo <zezouniverse7@gmail.com>
2023-07-16 21:24:01 +0000

    push it

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Descrubtion: using Size function
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	printf("Size of a char",sizeof(char));
+	printf("Size of a int",sizeof(int));
+	printf("Size of a long int",sizeof(long int));
+	printf("Size of a long long int",sizeof(long long int));
+	printf("Size of a float",sizeof(float));
+	return (0);
+}
