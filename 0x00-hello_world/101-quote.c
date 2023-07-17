950350920e434cbfa9483a15e53de56806de086c
zezomo <zezouniverse7@gmail.com>
2023-07-16 23:32:34 -0700

    dawd wf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
 */
 
 int main(void)
-{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	fwrite(1, quo, 59);
+{	char qu[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, qu, 59);
 	return (1);
 }
