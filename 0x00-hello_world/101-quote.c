aba97b12f5ce08dbbaec306e4a41fce3f83bdfbd
zezomo <zezouniverse7@gmail.com>
2023-07-16 23:30:19 -0700

    adsd

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,11 @@
  *
  * Descrubtion: using Size function
  *
- * Return: Always 1
+ * Return: 1 (error)
 */
 
 int main(void)
 {	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1, quo, 59);
+	fwrite(1, quo, 59);
 	return (1);
 }
