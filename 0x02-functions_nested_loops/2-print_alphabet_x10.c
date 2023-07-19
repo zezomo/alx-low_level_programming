aae83fed716f617a2318a24d8841d685266d7c69
zezomo <zezouniverse7@gmail.com>
2023-07-19 12:31:37 -0700

    pushed

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,9 @@ void print_alphabet_x10(void)
 {
 	int ln, ch;
 
-	for (ln = 0, ln <= 9, ln++)
+	for (ln = 0; ln <= 9; ln++)
 	{
-		for (ch = 'a', ch <= 'z', ch++)\
+		for (ch = 'a'; ch <= 'z'; ch++)
 			_putchar(ch);
 		_putchar('\n');
 	}
