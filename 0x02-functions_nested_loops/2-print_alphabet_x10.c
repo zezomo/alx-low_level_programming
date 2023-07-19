1e9512c59f3facb01ae4fa4530c401e25a6c30ff
zezomo <zezouniverse7@gmail.com>
2023-07-19 12:27:58 -0700

    pushed

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * 
+ * print_alphabet_x10
+ *
+*/
+
+
+void print_alphabet_x10(void);
+{
+	int ln, ch;
+
+	for (lne =0, ln <= 9, ln++)
+	{
+		for (ch = 'a', ch <= 'z', ch++)\
+			_putchar(ch);
+		_putchar('\n');
+	}
+
+
+
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,5 +4,5 @@
 #include <stdio.h>
 int _putchar(char s);
 void print_alphabet(void);
-
+void print_alphabet_x10(void);
 #endif
