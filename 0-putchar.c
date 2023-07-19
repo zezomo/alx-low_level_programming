7c0e8f843f5a0a48d156e383be11989da7452f6a
zezomo <zezouniverse7@gmail.com>
2023-07-19 11:16:25 -0700

    dasdf

---
+++ 0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Descrubtion: using Size function
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+        char arr[] = "_putchar";
+        int ch;
+
+        for (ch = 0; ch < 8; ch++)
+                _putchar(arr[ch]);
+        _putchar("\n");
+        return (0);
+
+
+}
