5162695377101e510f6c868a614a3d36b544576e
zezomo <zezouniverse7@gmail.com>
2023-07-19 11:18:45 -0700

    dwd

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Descrubtion: using Size function
+ *
+ * Return: Always 0
+*/
+
+int _putchar(char s)
+{
+        return (write(1, &, 1));
+}
