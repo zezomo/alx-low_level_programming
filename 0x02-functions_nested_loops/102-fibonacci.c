5f12754bede3e3e18aa947f5c32479a831be4950
zezomo <zezouniverse7@gmail.com>
2023-07-21 10:54:52 -0700

    pushed

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ * dgfwfgnawn;enwfoabnfawbnfbowe4rfoawrjfbkljabsjklfobaf
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int c;
+	int f1, f2, sum;
+
+	for (c = 0; c < 50; c++)
+	{
+		sum = f1 + f2;
+		printf("%d", sum);
+
+		f1 = f2;
+		f2 = sum;
+
+		if (c == 50)
+			printf("\n");
+		else
+			print(". ");
+	}
+	return (0);
+
+}
