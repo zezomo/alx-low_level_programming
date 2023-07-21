7689deb2568bc916c03209ed6b880237e593f677
zezomo <zezouniverse7@gmail.com>
2023-07-21 05:20:44 -0700

    pushed

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,10 +14,10 @@ void print_to_98(int n)
 	int c;
 
 	if (n > 98)
-		for (c = 0; c < 98; c--)
+		for (c = 0; c > 98; c--)
 			printf("%d, ", c);
 	else
-		for (c = 0; c > 0; c++)
+		for (c = 0; c < 0; c++)
 			printf("%d, ", c);
 	printf("98\n");
 
