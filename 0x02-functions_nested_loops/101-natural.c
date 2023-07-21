127a4712d575cb63ca244ef2bf9098488535c9ad
zezomo <zezouniverse7@gmail.com>
2023-07-21 06:29:42 -0700

    pushed

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int sum, i;
+
+	for (i = 0, i <= 1024, i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum = sum + i;
+
+
+
+
+
+	printf("%d", sum);
+}
