599aa688db21d869770e6d171a32b8aec276b952
zezomo <zezouniverse7@gmail.com>
2023-07-21 05:17:40 -0700

    pushed

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_to_98 - dcw dwd wd wdw dw dwdw wdheck the code
+ *
+ * @n :dwd wqd WD ADW DW DAWED DFW AWD FW FAW F
  *
  * Return: Always 0.
  */
@@ -12,10 +14,10 @@ void print_to_98(int n)
 	int c;
 
 	if (n > 98)
-		for (c=0; c < 98; c--)
+		for (c = 0; c < 98; c--)
 			printf("%d, ", c);
 	else
-		for (c=0; c > 0; c++)
+		for (c = 0; c > 0; c++)
 			printrf("%d, ", c);
 	printf("98\n");
 
