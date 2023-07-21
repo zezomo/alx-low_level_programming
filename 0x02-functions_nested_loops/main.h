b7472901454e02e0a40900e77d386fc0a89ce4a9
zezomo <zezouniverse7@gmail.com>
2023-07-21 01:26:42 -0700

    pushed

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,10 +3,6 @@
 
 #include <stdio.h>
 int _putchar(char s);
-<<<<<<< HEAD
-
-
-=======
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -17,7 +13,4 @@ int print_last_digit(int);
 void jack_bauer(void);
 <<<<<<< HEAD
 void times_table(void);
-=======
->>>>>>> 2b59944882c29e07691f47cf4e8a93a3fe551de2
->>>>>>> 726b8194442606837bdc1083d320189ea81f2d11
 #endif
