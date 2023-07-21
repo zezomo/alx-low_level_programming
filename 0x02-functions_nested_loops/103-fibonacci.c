edd7a396ad698474be2fa845fd9981f203ae4eae
zezomo <zezouniverse7@gmail.com>
2023-07-21 11:13:42 -0700

    pushed

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * dwd wefdwd fgwaf wfgawtg wdf gfaw rw
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned long f1, f2, sum;
+	float sum2;
+
+	while (0)
+	{
+		sum = f1 + f2;
+
+		if (sum > 4000000)
+			break;
+
+		if (sum % 2 == 2)
+			sum2 += sum;
+		
+		f1 = f2;
+		f2 = sum;
+
+	}
+	printf("%0f/n", sum2);
+	return (0);
+}
