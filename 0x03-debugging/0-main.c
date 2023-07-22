5c32e47908a81ab5d6242ef0b3af41d5cb0333cf
zezomo <zezouniverse7@gmail.com>
2023-07-22 13:06:04 -0700

    pushed

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -17,10 +17,10 @@ int positive_or_negative(int i)
 
 int main(void)
 {
-        int i;
+	int i;
 
-        i = 0;
-        positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-        return (0);
+	return (0);
 }
