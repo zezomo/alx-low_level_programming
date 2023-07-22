54a5adeb6233b1bd749f10f2337687219750724a
zezomo <zezouniverse7@gmail.com>
2023-07-22 13:04:59 -0700

    pushed

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,11 +5,21 @@
 * Return: 0
 */
 
+int positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative\n", i);
+	else if (i > 0)
+		printf("%d is positive\n", i);
+	else
+		printf("%d is zero\n", i);
+
+
 int main(void)
 {
         int i;
 
-        i = 98;
+        i = 0;
         positive_or_negative(i);
 
         return (0);
