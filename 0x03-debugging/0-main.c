fc7df47261f1b3c234071765bc73e4433b5b5834
zezomo <zezouniverse7@gmail.com>
2023-07-22 13:20:31 -0700

    pushed

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,19 +2,9 @@
 
 /**
 * main - tests function that prints if integer is positive or negative
-* positive_or_negative :dfw rfa wdfw amfafgn wonffnwnf  
-* @i df af nwwd adad faw fwa ff f afwd wa fa f
-* Return: 0
+*
+* return : 0
 */
-
-int positive_or_negative(int i)
-{
-	if (i < 0)
-		printf("%d is negative\n", i);
-	else if (i > 0)
-		printf("%d is positive\n", i);
-	else
-		printf("%d is zero\n", i);
 
 
 int main(void)
