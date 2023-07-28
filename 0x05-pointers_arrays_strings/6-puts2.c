d93c8d08b2822856b5a36590dfc0a67658f4e4df
zezomo <zezouniverse7@gmail.com>
2023-07-28 12:03:30 -0700

    pushed 3

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - check the codefawdf fa fawfdw fa fwf f fwaffawf
+ *
+ * @str: adfwfw afgawf wadfa f wfaf wfdaw wd dawd eaegesgegeg
+ *
+ * Return: Always 0.
+ */
+
+void puts2(char *str);
+{
+	int i;
+	for (i =0; str[i] != '\n'; ++i)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i];
+	}
+	_putchar('\n');
+}
