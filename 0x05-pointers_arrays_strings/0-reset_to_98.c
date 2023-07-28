592751f74cd99801417d134ca4fa246a4a2945a3
zezomo <zezouniverse7@gmail.com>
2023-07-28 09:59:30 -0700

    pushed 3

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - check the codefawdf fa fawf
+ *
+ * @n:  gsgesfawef f f adfwfw afgawf wadfa f wfaf wfafwsf af
+ *
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
