a806783ca970d36894a6795a15b0136f0966049c
zezomo <zezouniverse7@gmail.com>
2023-07-28 10:35:20 -0700

    pushed 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - check the codefawdf fa fawfdw fa fwf f fwaffawf
+ *
+ * @str: adfwfw afgawf wadfa f wfaf wfdaw wd dawd eaegesgegeg
+ *
+ * Return: Always 0.
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putshar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
+}
