79ceadec1b188146f6adabfb940df31c095f1dca
zezomo <zezouniverse7@gmail.com>
2023-07-28 10:45:07 -0700

    pushed 3

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - check the codefawdf fa fawfdw fa fwf f fwaffawf
+ *
+ * @s: adfwfw afgawf wadfa f wfaf wfdaw wd dawd eaegesgegeg
+ *
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	while (s[i])
+	{
+		i++;
+	}
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
