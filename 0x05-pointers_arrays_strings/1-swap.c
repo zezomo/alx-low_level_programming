45c0ed00985ff59a7d2e9e685661aee3eb75bb08
zezomo <zezouniverse7@gmail.com>
2023-07-28 10:11:16 -0700

    pushed 3

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - check the codefawdf fa fawfdw fa fwf f fwaffawf
+ *
+ * @a: adfwfw afgawf wadfa f wfaf wfdaw wd dawd eaegesgegeg
+ *
+ * @b: hhs ege g e  aefhgegwsg sghegsgae ghhaeghaeggeageg
+ *
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+
+}
