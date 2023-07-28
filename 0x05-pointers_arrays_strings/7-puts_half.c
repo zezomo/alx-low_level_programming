cf7fa899d556db3b2163b334f7780aca447cf984
zezomo <zezouniverse7@gmail.com>
2023-07-28 12:14:56 -0700

    pushed 3

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - check the codefawdf fa fawfdw fa fwf f fwaffawf
+ *
+ * @str: adfwfw afgawf wadfa f wfaf wfdaw wd dawd eaegesgegeg
+ *
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+		;
+	i++;
+
+	for (i /= 2; str[i] != '\n'; i++)
+		_putchar(str[i]);
+	_putchar(str[i]);
+}
