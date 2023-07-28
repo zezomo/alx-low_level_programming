51a321de5d32a97406f84e6739bf32ad3e18eb97
zezomo <zezouniverse7@gmail.com>
2023-07-28 12:40:55 -0700

    pushed 3

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - check the codefawdf fa fawfdw fa fwf f fwaffawf
+ *
+ * @dest :f afwf afawfaff gawg gawg g agaw g ssfafgagawgawggsgsfsf
+ *
+ * @src : adfwfw afgawf wadfa f wfaf wfdaw wd dawd eaegesgegeg
+ *
+ * Return: Always 0.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
