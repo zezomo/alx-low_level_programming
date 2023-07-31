a8ca4e1724cd66a23ff90df94c19299b0b0e09e5
zezomo <zezouniverse7@gmail.com>
2023-07-31 05:59:20 -0700

    pushed 3

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncat - check the codefawdf fa fawfdw fa fwf f fwaffawf
+ *
+ * @dest :f afwf afawfaff gawg gawg g agaw g ssfafgagawgawggsgsfsf
+ *
+ * @src :f afwf afawfaff gawg gawg g agaw g ssfafgagawgawggsgsfsf
+ *
+ * @ n : efgwggwegeetgghghhrhrherhweryhwertWEfwefwefwefefFefrffef 
+ *
+ * Return: Always 0.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int s1, s2;
+
+	s1 = 0;
+
+	while (dest[s1])
+		s1++;
+
+	for (s2 = 0; s2 < n && src[s2] ; s2++)
+		dest[s1 + s2] = src[s2];
+
+	dest[s1 + s2] = '\0';
+	return (dest);
+}
