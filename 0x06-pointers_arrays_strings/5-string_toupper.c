4427cbb9349bfa06674bb61dbd3926e985ba2777
zezomo <zezouniverse7@gmail.com>
2023-07-31 07:00:02 -0700

    pushed for life

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - check the codefawdf fa fawfdw fa fwf f fwaffawf
+ *
+ * @co : awdw dawdaw f faw aeg ag gae waafwdfawfawf afaw da
+ *
+ * Return: Always 0.
+ */
+
+char *string_toupper(char *co)
+{
+	int i;
+
+	for (i = 0; co[i] != '\0'; i++)
+	{
+		if (co[i] >= 97 && co[i] <= 122)
+		{
+			co[i] = co[i] = 32;
+		}
+	}
+
+	return (co);
+}
