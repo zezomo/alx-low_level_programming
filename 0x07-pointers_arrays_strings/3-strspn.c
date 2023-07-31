a97f328dd41bbbedbe49f27d3dc69cd51a333b81
zezomo <zezouniverse7@gmail.com>
2023-07-31 14:04:30 -0700

    pushed for life

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,13 +13,13 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i , j;
+	unsigned int i, j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != s[i]; j++)
 		{
-			if (accept != '\0')
+			if (accept[j] == '\0')
 				return (i);
 		}
 	}
