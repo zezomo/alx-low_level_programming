4d7c2b9348e8d116333b8ff019d79412293be2e8
zezomo <zezouniverse7@gmail.com>
2023-07-31 06:20:33 -0700

    pushed 3

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcmp - check the codefawdf fa fawfdw fa fwf f fwaffawf
+ *
+ * @a :f afwf afawfaff gawg gawg g agaw g ssfafgagawgawggsgsfsf
+ *
+ * @n :f afwf afawfaff gawg gawg g agaw g ssfafgagawgawggsgsfsf
+ *
+ * Return: Always 0.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int j, i;
+	char tmp;
+
+	for (j = 0; s[j] != '\0'; ++j)
+		;
+
+	for (i = 0; i < j / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[j - 1 - i];
+		s[j - 1 - i] = tmp;
+	}
+}
