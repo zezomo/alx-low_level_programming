b7e18caa8bbbf836c282fe5996a35ba149ab71da
zezomo <zezouniverse7@gmail.com>
2023-07-31 14:20:56 -0700

    pushed for life

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - check the codefawdf fa fawfdw fa fwf f fwaffawf
+ *
+ * @haystack : da dw daf fg faw faf adffawdfawdf faw fwfaw gaega f
+ *
+ * @needle : d faw g agh fgweafadfas fw agagw fawd adf afwf af
+ *
+ * Return: Always 0.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+		int i;
+
+		while (*cc)
+		{
+			for (i = 0; accept[i]; i++)
+			{
+			if (*cc == accept[i])
+			return (cc);
+			}
+		cc++;
+		}
+
+	return ('\0');
+}
