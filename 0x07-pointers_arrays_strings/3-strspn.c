07a2bc40f1e57ca74d2d8cd7cdd747ad2250fd95
zezomo <zezouniverse7@gmail.com>
2023-07-31 13:53:51 -0700

    pushed for life

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - check the codefawdf fa fawfdw fa fwf f fwaffawf
+ *
+ * @s : da dw daf fg faw faf adffawdfawdf faw fwfaw gaega f
+ *
+ * @accept : d faw g agh fgweafadfas fw agagw fawd adf afwf af
+ *
+ * Return: Always 0.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i , j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != s[i]; j++)
+		{
+			if (accept != '\0')
+				return (i);
+		}
+	}
+	return (i);
+}
