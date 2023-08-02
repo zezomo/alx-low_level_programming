137161d8f5aa1be0aa351249a7f0a294f7b93900
zezomo <zezouniverse7@gmail.com>
2023-08-02 11:38:45 -0700

    pushed for life

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - check the codefawdf fa fawfdw fa fwf f fwaffawf
+ *
+ * @s : da dw daf fg faw faf adffawdfawdf faw fwfaw gaega f
+ *
+ * Return: Always 0.
+ */
+
+int _strlen_recursion(char *s)
+{
+	int c;
+
+	if (*s == '\0')
+	{
+		c += _strlen_recursion(s + 1) + 1;
+	}
+	return (c);
+}
