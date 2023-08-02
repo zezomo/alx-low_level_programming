6cedd10e79a964614c425558472e14963523f2ca
zezomo <zezouniverse7@gmail.com>
2023-08-02 12:33:03 -0700

    pushed for life

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _pow_recursion - check the codefawdf fa fawfdw fa fwf f fwaffawf
+ *
+ * @x : da dw daf fg faw faf adffawdfawdf faw fwfaw gaega f
+ *
+ * @y : da dw daf fg faw faf adffawdfawdf faw fwfaw gaega f
+ *
+ * Return: Always 0.
+ */
+
+int _pow_recursion(int x, int y)
+{
+
+}
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _sqrt_recursion - check the codefawdf fa fawfdw fa fwf f fwaffawf
+ *
+ * @n : da dw daf fg faw faf adffawdfawdf faw fwfaw gaega f
+ *
+ * Return: Always 0.
+ */
+
+int _sqrt_recursion(int n)
+{
+
+}
+
+
