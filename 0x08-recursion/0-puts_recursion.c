02136ee58740e91e1cf450f3b8cc7742077d1f4a
zezomo <zezouniverse7@gmail.com>
2023-08-02 11:25:51 -0700

    pushed for life

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - check the codefawdf fa fawfdw fa fwf f fwaffawf
+ *
+ * @s : da dw daf fg faw faf adffawdfawdf faw fwfaw gaega f
+ *
+ * Return: Always 0.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
+
+
