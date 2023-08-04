ef851658639ee565954861c0ff8f5432908cf3d3
zezomo <zezouniverse7@gmail.com>
2023-08-04 10:44:32 -0700

    d

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - check the codefawdf fa fawfdw fa fwf f fwaffawf
+ *
+ * @argv : da dw daf fg faw faf adffawdfawdf faw fwfaw gaega f
+ *
+ * @argc : da dw daf fg faw faf adffawdfawdf faw fwfaw gaega f
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char const *argv)
+{
+	int c = 0;
+
+	while (argc--)
+	{
+		printf("%d\n", argv[c]);
+		c++;
+	}
+	return (0);
+}
