ed4ab723c025cbe9f3a8f05f27b6b6e78876dbcf
zezomo <zezouniverse7@gmail.com>
2023-08-04 10:20:15 -0700

    d

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the codefawdf fa fawfdw fa fwf f fwaffawf
+ *
+ * @argv : da dw daf fg faw faf adffawdfawdf faw fwfaw gaega f
+ *
+ * @argc : da dw daf fg faw faf adffawdfawdf faw fwfaw gaega f
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[](void))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
