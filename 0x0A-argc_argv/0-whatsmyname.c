dd0c647886a1fa2f3b2c62023f39c8c5ebe9ffb8
zezomo <zezouniverse7@gmail.com>
2023-08-04 10:11:02 -0700

    d

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the codefawdf fa fawfdw fa fwf f fwaffawf
+ *
+ * @argv : da dw daf fg faw faf adffawdfawdf faw fwfaw gaega f
+ *
+ * @argc : da dw daf fg faw faf adffawdfawdf faw fwfaw gaega f
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+
+}
