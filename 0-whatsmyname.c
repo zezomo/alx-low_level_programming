a3ee6d7a2310b1a2ac87a4c86b5166576775f991
zezomo <zezouniverse7@gmail.com>
2023-08-11 13:50:14 -0700

    push it

---
+++ 0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * wildcmp - check the codefawdf fa fawfdw fa fwf f fwaffawf
+ *
+ * @argc : da dw daf fg faw faf adffawdfawdf faw fwfaw gaega f
+ *
+ * @argv : da dw daf fg faw faf adffawdfawdf faw fwfaw gaega f
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
