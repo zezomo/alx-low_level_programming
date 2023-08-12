97fd8d88b1bb9d2c72ea8658e4227e60b4dd58be
zezomo <zezouniverse7@gmail.com>
2023-08-12 05:33:49 -0700

    pushed for life

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - check the codefawdf fa fawfdw fa fwf f fwaffawf
+ *
+ * @width : da dw daf fg faw faf adffawdfawdf faw fwfaw gaega f
+ *
+ * @height : da dw daf fg faw faf adffawdfawdf faw fwfaw gaega f
+ *
+ * Return: Always 0.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **sel, i, j;
+	sel = malloc(sizeof(*sel) * height);
+
+	if (width <= 0 || height <= 0 || *sel == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < height; i++)
+		{
+			sel[i] = malloc(sizeof(**sel) * width);
+
+			for (j = 0; j < width; j++)
+				sel[i][j] = 0;
+		}
+	}
+	return (sel);
+}
