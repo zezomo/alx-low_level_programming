c5cf05696e40cf3a93bea81af1c5481dface351b
zezomo <zezouniverse7@gmail.com>
2023-08-12 06:44:10 -0700

    pushed for life

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - check the codefawdf fa fawfdw fa fwf f fwaffawf
+ *
+ * @grid : da dw daf fg faw faf adffawdfawdf faw fwfaw gaega f
+ *
+ * @height : da dw daf fg faw faf adffawdfawdf faw fwfaw gaega f
+ *
+ * Return: Always 0.
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	for (; i < height; i++)
+		free(grid[i]};
+	free(grid);
+}
