13b4c797a568985f3f5394acdb4ba3c23468220c
zezomo <zezouniverse7@gmail.com>
2023-08-12 00:05:23 -0700

    pushed for life

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * create_array - check the codefawdf fa fawfdw fa fwf f fwaffawf
+ *
+ * @size : da dw daf fg faw faf adffawdfawdf faw fwfaw gaega f
+ *
+ * @c : da dw daf fg faw faf adffawdfawdf faw fwfaw gaega f
+ *
+ * Return: Always 0.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ch = malloc(size);
+
+	if (ch == 0 || size == 0)
+		return (0);
+
+	while (size--)
+		ch[size] = c;
+	return (ch);
+
+}
