fed9494143518d61fff59d3ff960b6032f774f11
zezomo <zezouniverse7@gmail.com>
2023-08-17 16:28:33 -0700

    dafafg

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * sum_them_all - helafsfaf fa fa ffafafaf f afar awe faf aef afaf af fafaf
+ * 
+ * @n: f af f agaefgafadfafaf f f af f fwefaf f aef faefwef f aef f
+ *
+ * Return: number of words
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list nu;
+	unsigned int i;
+
+	va_start(nu, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(nu, int));
+
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+
+	va_end(nu);
+
+
+}
