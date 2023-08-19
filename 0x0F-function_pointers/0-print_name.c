0263f47a09fc45f75e63a6c3cb29240dd5edc71d
zezomo <zezouniverse7@gmail.com>
2023-08-19 05:00:40 -0700

    pushed for life

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,7 +3,7 @@
 
 /**
  * print_name - helafsfaf fa fa ffafafaf f afar awe faf aef afaf af fafaf
- * 
+ *
  * @name: f af f agaefgafadfafaf f f af f fwefaf f aef faefwef f aef f
  *
  * @f: f af f agaefgafadfafaf f f af f fwefaf f aef faefwef f aef f
@@ -12,7 +12,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f())
+	if (name && f)
 		f(name);
 
 }
