9b937ea5db34d5319184ea83c8206fa7b3b32635
zezomo <zezouniverse7@gmail.com>
2023-08-19 09:29:51 -0700

    pushed for life

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - helafsfaf fa fa ffafafaf f afar awe faf aef afaf af fafaf
+ *
+ * @s: f af f agaefgafadfafaf f f af f fwefaf f aef faefwef f aef f
+ *
+ * Return: number of words
+ */
+int main(int ca, char **cu)
+{
+	int (*fun)(int, int), x, y;
+
+	if (ca != 4)
+		printf("Error\n"), exit(98);
+
+	x = atoi(cu[1]);
+	y = atoi(cu[3]);
+
+	fun = get_op_function(cu[2]);
+
+	if (!fun)
+		printf("Error\n"), exit(99);
+	if (!y && (cu[2][0] == '/' || cu[2][0] == '%'))
+		printf("Error\n"), exit(100);
+	printf("%d\n", fun(x, y));
+	return (0);
+}
+
