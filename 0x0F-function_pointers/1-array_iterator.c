567805893655cffea5a8757491817a0e5615ef64
zezomo <zezouniverse7@gmail.com>
2023-08-19 06:07:38 -0700

    pushed for life

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,14 +7,16 @@
  * @array: f af f agaefgafadfafaf f f af f fwefaf f aef faefwef f aef f
  *
  * @size: f af f agaefgafadfafaf f f af f fwefaf f aef faefwef f aef f
- * @action: afwgafgwibgva bwafn oa awn w fnaf afaf w affgag w fawfg fafwf 
+ *
+ * @action: afwgafgwibgva bwafn oa awn w fnaf afaf w affgag w fawfg fafwf
+ *
  * Return: number of words
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	 int *end = array + size - 1;
+	int *end = array + size - 1;
 
-	 if (array && size && action)
+	if (array && size && action)
 		while (array <= end)
 			action(*array++);
 }
