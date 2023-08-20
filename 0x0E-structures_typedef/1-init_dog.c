b72ebdf4c8a242b10b3eddccc95813e9ea203183
zezomo <zezouniverse7@gmail.com>
2023-08-20 12:48:45 -0700

    push it

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * init_dog - helafsfaf fa fa ffafafaf f afar awe faf aef afaf af fafaf
+ *
+ * @d: f af f agaefgafadfafaf f f af f fwefaf f aef faefwef f aef f
+ *
+ * @name: f af f agaefgafadfafaf f f af f fwefaf f aef faefwef f aef f
+ *
+ * @age: f af f agaefgafadfafaf f f af f fwefaf f aef faefwef f aef f
+ *
+ * @owner: f af f agaefgafadfafaf f f af f fwefaf f aef faefwef f aef f
+ *
+ * Return: number of words
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}	
+}
