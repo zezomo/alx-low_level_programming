a3bf3c441229ef0b18f5ea40ad4a3d795d1dddb7
zezomo <zezouniverse7@gmail.com>
2023-08-20 09:14:02 -0700

    push it

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * STRUCT - helafsfaf fa fa ffafafaf f afar awe faf aef afaf af fafaf
+ *
+ * @name: f af f agaefgafadfafaf f f af f fwefaf f aef faefwef f aef f
+ *
+ * @age: f af f agaefgafadfafaf f f af f fwefaf f aef faefwef f aef f
+ *
+ * @owner: f af f agaefgafadfafaf f f af f fwefaf f aef faefwef f aef f
+ *
+ * Return: number of words
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
