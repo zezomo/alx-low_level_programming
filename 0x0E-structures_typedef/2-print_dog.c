c7fcb0d758ec92e7032c0ef99602927beadf87d6
zezomo <zezouniverse7@gmail.com>
2023-08-20 13:27:21 -0700

    push it

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "dog.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_dog - helafsfaf fa fa ffafafaf f afar awe faf aef afaf af fafaf
+ *
+ * @d: f af f agaefgafadfafaf f f af f fwefaf f aef faefwef f aef f
+ *
+ * Return: number of words
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("%s\n", d->name ? d->name : "nil");
+		printf("%s\n", d->age ? d->age : "nil");
+		printf("%s\n", d->owner ? d->owner : "nil");
+	}
+}
+
