f55d53a3f9632378fea5f0a1576f8b50b12c305f
zezomo <zezouniverse7@gmail.com>
2023-08-20 12:56:15 -0700

    push it

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+
+
 /**
  * struct dog - helafsfaf fa fa ffafafaf f afar awe faf aef afaf af fafaf
  *
