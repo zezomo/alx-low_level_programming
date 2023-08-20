8ec04e8cae7784a229b7a5298971427baa55871e
zezomo <zezouniverse7@gmail.com>
2023-08-20 13:36:02 -0700

    push it

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,9 +13,9 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		printf("name: %s\n", d->name ? d->name : "nil");
-		printf("age: %f\n", d->age);
-		printf("owner: %s\n", d->owner ? d->owner : "nil");
+		printf("Name: %s\n", d->name ? d->name : "nil");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "nil");
 	}
 }
 
