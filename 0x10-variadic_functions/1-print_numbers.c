2fdc38dc0fc7ea8d26983cb6b8f2fd1400e05c5c
zezomo <zezouniverse7@gmail.com>
2023-08-20 01:51:40 -0700

    dafafg

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * print_numbers - helafsfaf fa fa ffafafaf f afar awe faf aef afaf af fafaf
+ *
+ * @separator: f af f agaefgafadfafaf f f af f fwefaf f aef faefwef f aef f
+ *
+ * @n: f af f agaefgafadfafaf f f af f fwefaf f aef faefwef f aef f
+ *
+ * Return: number of words
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i = n;
+	va_list(arg);
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(arg, n);
+
+	while (i--)
+		printf("%d%s", va_arg(arg, int),
+				i ? (separator ? separator : "") : "\n");
+	va_end(arg);
+
+}
+
