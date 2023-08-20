bdcbeeea19aafd2bcbe69f70a952c93369a54adf
zezomo <zezouniverse7@gmail.com>
2023-08-20 02:34:44 -0700

    dafafg

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include "ariadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_all - helafsfaf fa fa ffafafaf f afar awe faf aef afaf af fafaf
+ *
+ * @format: f af f agaefgafadfafaf f f af f fwefaf f aef faefwef f aef f
+ *
+ * Return: number of words
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *str, *sep = "";
+
+	va_list list;
+
+	va_start(list, format);
+
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(list, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(list, double));
+					break;
+				case 's':
+					str = va_arg(list, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
+		}
+	}
+
+	printf("\n");
+	va_end(list);
+
+}
+
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,20 @@
 #ifndef VARIADIC_H
 #define VARIADIC_H
 
+#include <stdlib.h>
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+typedef struct token
+{
+	char *token;
+	void (*f)(char *, va_list);
+} token_t;
+
+
+
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
