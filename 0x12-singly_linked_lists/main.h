2370f5e7eb644b1ff4f41c022d96e3f7749ad800
zezomo <zezouniverse7@gmail.com>
2023-08-25 08:34:14 -0700

    from zezo

---
+++ 0x12-singly_linked_lists/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* LIBRARIES */
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+
+/* FUNCTIONS */
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+
+#endif
