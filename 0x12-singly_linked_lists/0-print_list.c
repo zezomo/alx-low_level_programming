b549812d5e427f82b5d0b4591225a0249f0b7194
zezomo <zezouniverse7@gmail.com>
2023-08-28 06:12:03 -0700

    from zezo

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -18,7 +18,7 @@ int _strlen(char *s)
 /**
  * print_list - helafsfaf fa fa ffafafaf f afar awe faf aef afaf af fafaf
  *
- * @h: f af f agaefgafadfafaf f f af f fwefaf f aef faefwef f aef f
+ *@h: f af f agaefgafadfafaf f f af f fwefaf f aef faefwef f aef f
  *
  * Return: number of words
  */
