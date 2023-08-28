9757c54955b0a0f9fbea9cf355856e8e92f3b6a6
zezomo <zezouniverse7@gmail.com>
2023-08-28 05:59:45 -0700

    from zezo

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * _strlen - calulate the length of string
+ * Description: it calulate the string passed to _puts
+ * @s: '*s' is a pointer
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length])
+		length++;
+
+	return (length);
+}
+
+#include "main.h"
+
+/**
+ * print_list - helafsfaf fa fa ffafafaf f afar awe faf aef afaf af fafaf
+ *
+ * @h: f af f agaefgafadfafaf f f af f fwefaf f aef faefwef f aef f
+ *
+ * Return: number of words
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "nil");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
--- 0x12-singly_linked_lists/main.h
+++ 0x12-singly_linked_lists/main.h
@@ -8,13 +8,6 @@
 #include <stdio.h>
 #include <limits.h>
 
-/* FUNCTIONS */
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -29,5 +22,15 @@ typedef struct list_s
     unsigned int len;
     struct list_s *next;
 } list_t;
+
+
+/* FUNCTIONS */
+int _putchar(char s);
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
