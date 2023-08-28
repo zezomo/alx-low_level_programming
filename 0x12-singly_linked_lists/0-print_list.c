ae7337f5f922751160b6b01f026370614a7a8dfe
zezomo <zezouniverse7@gmail.com>
2023-08-28 06:46:20 -0700

    from zezo

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,21 +1,6 @@
 #include "lists.h"
 
 /**
- * _strlen - calulate the length of string
- * Description: it calulate the string passed to _puts
- * @s: '*s' is a pointer
- * Return: length of string
- */
-int _strlen(char *s)
-{
-	int length = 0;
-
-	while (s[length])
-		length++;
-
-	return (length);
-}
-/**
  * print_list - helafsfaf fa fa ffafafaf f afar awe faf aef afaf af fafaf
  *
  * @h: f af f agaefgafadfafaf f f af f fwefaf f aef faefwef f aef f
