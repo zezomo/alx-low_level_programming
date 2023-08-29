fe2e91cc77632ee5075ef3c9af05e14f853d11d5
zezomo <zezouniverse7@gmail.com>
2023-08-29 05:40:30 -0700

    from zezo

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_list - helafsfaf fa fa ffafafaf f afar awe faf aef afaf af fafaf
+ *
+ * @head: f af f agaefgafadfafaf f f af f fwefaf f aef faefwef f aef f
+ *
+ * Return: number of words
+ */
+void free_list(list_t *head)
+{
+	list_t *node, *next_node;
+	
+	if (!head)
+		return;
+
+	node = head;
+	while (node)
+	{
+		next_node = node->next;
+		free(node->next);
+		free(node);
+		node = next_node;
+	}
+
+}
