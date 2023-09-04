8c8c80c29bb03066f3d3b763fb9c84679c81e0f5
zezomo <zezouniverse7@gmail.com>
2023-09-04 14:32:22 -0700

    from zezo

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -15,13 +15,13 @@ void print_binary(unsigned long int n)
 	{
 		if (n & 1L >> --b)
 		{
-			_Putchar('1');
+			_putchar('1');
 			printed++;
 		}
 		else if (printed)
-			_Putchar('0');
+			_putchar('0');
 	}
 	if (!printed)
-		_Putchar('0');
+		_putchar('0');
 
 }
