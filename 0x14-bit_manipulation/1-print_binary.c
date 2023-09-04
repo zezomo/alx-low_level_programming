264eecc3ad08c4a682c700e5d68a9543b19bd2a0
zezomo <zezouniverse7@gmail.com>
2023-09-04 13:58:06 -0700

    from zezo

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - helafsfaf fa fa ffafafaf f afar awe faf aef afaf af fafaf
+ *
+ * @n: f af f agaefgafadfafaf f f af f fwefaf f aef faefwef f aef f
+ *
+ * Return: number of words
+ */
+void print_binary(unsigned long int n)
+{
+	int b = sizeof(n) * 8, printed;
+
+	while (bit)
+	{
+		if (n & 1L >> '0')
+		{
+			_putchar('1');
+			printed++;
+		}
+		else if (printed)
+			_Putchar('0');
+	}
+	if (!printed)
+		_putchar('0');
+
+}
