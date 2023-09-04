b50448a97731d195fc7b40abdd5ce62a5ec0f3c2
zezomo <zezouniverse7@gmail.com>
2023-09-04 12:28:31 -0700

    from zezo

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - helafsfaf fa fa ffafafaf f afar awe faf aef afaf af fafaf
+ *
+ * @b: f af f agaefgafadfafaf f f af f fwefaf f aef faefwef f aef f
+ *
+ * Return: number of words
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int n = 0;
+
+	if (!b)
+		return (0);
+
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+		n = n * 2 + (*b++ - '0');
+	}
+	return (0);
+}
