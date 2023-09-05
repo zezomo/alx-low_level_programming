395420aad042e2926b45dc1b4e73f84489a8c014
zezomo <zezouniverse7@gmail.com>
2023-09-04 17:48:47 -0700

    from zezo

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -3,10 +3,6 @@
 /**
  * get_endianness - helafsfaf fa fa ffafafaf f afar awe faf aef afaf af fafaf
  *
- * @n: f af f agaefgafadfafaf f f af f fwefaf f aef faefwef f aef f
- *
- * @m: f af f agaefgafadfafaf f f af f fwefaf f aef faefwef f aef f
- *
  * Return: number of words
  */
 int get_endianness(void)
