cd5ed6574ae03352af99468ebcbf98d2da509b44
zezomo <zezouniverse7@gmail.com>
2023-09-28 13:17:24 -0700

    zezo

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * clear_bit - helafsfaf fa fa ffafafaf f afar awe faf aef afaf af fafaf
+ *
+ * @n: f af f agaefgafadfafaf f f af f fwefaf f aef faefwef f aef f
+ *
+ * @index: gwge hfvev heov hoef gve gfgwfw fwefeg effgwefa faefwf
+ *
+ * Return: number of words
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask = 1UL << index;
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+
+	mask = ~mask;
+	*n &= mask;
+
+	return (1);
+}
+
