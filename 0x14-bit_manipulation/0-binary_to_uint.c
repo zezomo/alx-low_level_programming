9b94f7160e1da170e4a892da5b0a84b6f947a371
zezomo <zezouniverse7@gmail.com>
2023-09-28 10:42:22 -0700

    zezo

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,21 +10,21 @@
 size_t binary_to_uint(const list_t *h)
 {
 	if (b == NULL)
-		return 0;
+		return (0);
 
 	unsigned int result = 0;
 	int i = 0;
 
-	while (b[i] != '\0') {
-	// Check if the character is not '0' or '1'
-		if (b[i] != '0' && b[i] != '1') {
-			return 0; // Invalid character found, return 0
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
 		}
 
-	// Convert binary to unsigned int
 		result = (result << 1) + (b[i] - '0');
 		i++;
 	}
 
-	return result;
+	return (result);
 }
