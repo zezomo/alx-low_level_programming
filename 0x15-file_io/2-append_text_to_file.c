6536ce9f38f3d09d41b0d314bcbcc0f039bad0d5
zezomo <zezouniverse7@gmail.com>
2023-10-02 17:13:52 -0700

    zezo

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - helafsfaf fa fa ffafafaf f afar awe faf aef afaf af fafaf
+ *
+ * @filename : vohewivbeivbe ieheiybw cbiywebcw ibeyvbweivy ibveyib
+ *
+ * @text_content : dveneiv nciunwiucn cniucnwiuncw vnwcinwi wi cwwnc wcn
+ *
+ * Return: number of words
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	ssize_t bytes_written =
+		write(file_descriptor, text_content, strlen(text_content));
+	int file_descriptor = open(filename, O_WRONLY | O_APPEND);
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		return (1);
+
+	if (file_descriptor == -1)
+		return (-1);
+
+	if (bytes_written == -1)
+	{
+		close(file_descriptor);
+		return (-1);
+	}
+
+	close(file_descriptor);
+	return (1);
+}
+
